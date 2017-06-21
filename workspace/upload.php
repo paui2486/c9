1a8a85d4ff84e24f8440d770a0b284c863a7247e
paui <paul@75.media>
2017-06-21 11:37:21 +0800

    基礎上傳更多判斷完成

--- workspace/upload.php
+++ workspace/upload.php
@@ -11,6 +11,8 @@
 
 $target_dir = "/home/ubuntu/workspace/workspace/";//存檔路徑
 
+
+
 if($_FILES['uploadFile']['error']>0){//檢測是否有錯誤
   echo ("檔案上傳失敗！");//如果出現錯誤則停止程式
   echo "Error: " . $_FILES["file"]["error"];
@@ -21,35 +23,46 @@ if($_FILES['uploadFile']['error']>0){//檢測是否有錯誤
 $temp = explode(".", $_FILES["uploadFile"]["name"]);//explode把檔名跟副檔名用 " . “分開
 $extension = end($temp);//end選取array最後一個元素也就是副檔名
 
-if($extension != "xlsx"){
+if($extension != "xlsx"){ //副檔名判斷
     echo ("檔案上傳失敗！");//如果出現錯誤則停止程式
   echo "錯誤:這不是excel或是非xlsx副檔名<br>";
-  echo $extension;
-  //echo "Type: ". $_FILES['uploadFile']['type'] . "<br>";
+  //echo $extension;
   echo("<meta http-equiv=REFRESH CONTENT=5;url=up.html>");//5秒後跳轉up.html
   exit;
 }
 
-/*if($_FILES['uploadFile']['type'] != 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'){ //檢測檔案類型 MIME
+if($_FILES['uploadFile']['type'] != 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'){ //檢測檔案類型 MIME判斷
   echo ("檔案上傳失敗！");//如果出現錯誤則停止程式
   echo "錯誤:這不是excel<br>";
-  echo "Type: ". $_FILES['uploadFile']['type'] . "<br>";
-  //echo("<meta http-equiv=REFRESH CONTENT=5;url=up.html>");//5秒後跳轉up.html
+  echo("<meta http-equiv=REFRESH CONTENT=5;url=up.html>");//5秒後跳轉up.html
   exit;
-}*/
+}
 
 echo "Filename: " . $_FILES['uploadFile']['name'] . "<br>";//案在客戶端電腦上的檔案名稱
 echo "Temporary Name: " . $_FILES['uploadFile']['tmp_name'] . "<br>";//上傳檔案儲存在伺服器端的暫存檔案名
-echo "Size: ". $_FILES['uploadFile']['size']/1024 . "<br>";//上傳檔案的檔案大小
+echo "Size: ". $_FILES['uploadFile']['size']/1024 . "KB<br>";//上傳檔案的檔案大小
 echo "Type: ". $_FILES['uploadFile']['type'] . "<br>";//檔案的 MIME 類型
+
+//檢查檔案是否重複 根據檔案位置 名稱
+if (file_exists("$target_dir" . $_FILES["uploadFile"]["name"])){
+    echo "檔案已經存在，請勿重覆上傳相同檔案";
+    echo "五秒後跳轉";
+    echo("<meta http-equiv=REFRESH CONTENT=5;url=up.html>");//5秒後跳轉up.html
+    exit;
+}
+
 
 // copy file here
 if (@copy($_FILES['uploadFile']['tmp_name'], "$target_dir" . $_FILES['uploadFile']['name'])) {
-    echo "<b>File successfully upload</br>";
+    echo "<b>檔案上傳成功</br>";
     echo "$target_dir" . $_FILES['uploadFile']['name'];
+    $newxlsx = $_FILES['uploadFile']['name'];
+    //include("match6.php");
 } else {
-    echo "<b>Error: failed to upload file</br>";
+    echo "<b>Error: 檔案上傳失敗</br>";
     echo "Error: " . $_FILES["file"]["error"];
+    echo("<meta http-equiv=REFRESH CONTENT=5;url=up.html>");//5秒後跳轉up.html
+    exit;
 }
 ?>
  
