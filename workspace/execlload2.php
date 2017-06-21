f7ba6a8feb8fd59549619fa93abecef4aa110c5a
paui <paul@75.media>
2017-06-21 09:47:33 +0800

    我也不清楚XD

--- workspace/execlload2.php
+++ workspace/execlload2.php
@@ -6,7 +6,7 @@ require_once('/home/ubuntu/workspace/workspace/PHPExcel/Classes/PHPExcel.php');/
 require_once('/home/ubuntu/workspace/workspace/PHPExcel/Classes/PHPExcel/Writer/Excel2007.php');// 輸出用
 require_once('/home/ubuntu/workspace/workspace/PHPExcel/Classes/PHPExcel/IOFactory.php');//讀取用
 //檔案路徑
-$inputFileName = 'filename.xls';
+$inputFileName = 'FV.xlsx';
 
 $reader = PHPExcel_IOFactory::createReader('Excel2007'); // 讀取2007 excel 檔案
 $PHPExcel = $reader->load($inputFileName); // 檔案名稱 需已經上傳到主機上
@@ -16,10 +16,15 @@ $sheet = $PHPExcel->getSheet(0); // 讀取第一個工作表(編號從 0 開始)
 $HighestColumn = $sheet->getHighestColumn(); //最大欄位的英文代號?
 $highestRow = $sheet->getHighestRow(); // 取得總列數 數字編號。A=0, B=1, C=2
 
+$TotalColumn = (ord($HighestColumn)-(64));// 將"任意"字符轉換為 ASCII 碼: 這裡是將G轉換71-64=7 
+
+//echo '總共 '.$HighestColumn;
+
 echo '總共 '.$highestRow.' 列';
+echo '總共 '.$TotalColumn.' 行';
 // 一次讀取一列
 for ($row = 0; $row <= $highestRow; $row++) {
-    for ($column = 0; $column <= 1; $column++) {//看你有幾個欄位 此範例為 13 個位
+    for ($column = 0; $column <= $TotalColumn; $column++) {//看你有幾個欄位
         $val = $sheet->getCellByColumnAndRow($column, $row)->getValue();
         echo $val.' ';
     }
