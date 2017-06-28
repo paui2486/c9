673b246dd79ed13641575606e586eb2473a87b89
paui <paul@75.media>
2017-06-28 13:14:31 +0800

    連線類別化

---
+++ workspace/PDOCON.php
@@ -0,0 +1,42 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<?php
+
+class Database //DB
+{
+    private static $dbName = 'c9' ;
+    private static $dbHost = 'localhost' ;
+    private static $dbUsername = 'paui';
+    private static $dbUserPassword = '28835328';
+     
+    private static $cont  = null;//清除狀態
+     
+    public function __construct() {
+        die('Init function is not allowed');
+    }
+     
+    public static function connect()
+    {
+       // One connection through whole application
+       if ( null == self::$cont )
+       {     
+        try
+        {
+          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName.";"."charset=utf8", self::$dbUsername, self::$dbUserPassword);  //charset
+          //$pdo = new PDO('mysql:host=localhost;dbname=c9;charset=utf8', 'paui', '28835328');
+
+        }
+        catch(PDOException $e)
+        {
+          die($e->getMessage()); 
+        }
+       }
+       return self::$cont;
+    }
+     
+    public static function disconnect()
+    {
+        self::$cont = null;
+    }
+}
+
+?>
